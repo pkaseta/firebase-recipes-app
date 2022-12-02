64d73df4193eb2c1f19138798007410af9c619a4
pkaseta <kasetapaul@gmail.com>
2022-12-01 21:30:29 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component }  from 'react';
+import firebase from './FirebaseConfig'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='title-row'>
+        <h1 className='title'>Firebase Recipes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
