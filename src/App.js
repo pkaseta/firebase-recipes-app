b84b76da988e1eb9e7309f0855fa0c7c8d234b70
pkaseta <kasetapaul@gmail.com>
2022-12-02 13:57:25 -0500

    added login form and updated .yml files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import React from 'react';
-// eslint-disable-next-line no-unused-vars
-import firebase from './FirebaseConfig'
+import React, {useState} from 'react';
+import FirebaseAuthService from './FirebaseAuthService';
+import LoginForm from './Components/LoginForm';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subscribetoAuthChanges(setUser)
+
   return (
     <div className="App">
       <div className='title-row'>
         <h1 className='title'>Firebase Recipes</h1>
+        <LoginForm existingUser={user}/>
       </div>
     </div>
   );
